4539fcb67a4ad78100ed0ebefbe983f2dda03c22
thisisraphie <raphielevita@gmail.com>
2025-02-14 13:25:00 +0800

    working? login screen :P

--- app/src/main/java/com/example/focusflow1/Login.java
+++ app/src/main/java/com/example/focusflow1/Login.java
@@ -1,6 +1,11 @@
 package com.example.focusflow1;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginButton;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        usernameInput = findViewById(R.id.username);
+        passwordInput = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(v -> {
+            String username = usernameInput.getText().toString().trim();
+            String password = passwordInput.getText().toString().trim();
+            Log.i("Test Credentials", "Username: " + username + ", Password: " + password);
         });
     }
 }
