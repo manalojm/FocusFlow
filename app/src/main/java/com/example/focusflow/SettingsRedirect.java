037a1c8116ab9ef8e048022fbe7b45e39abd3880
Centhor <vincenthizon266@gmail.com>
2025-03-14 14:01:16 +0800

    Popup of some image

---
+++ app/src/main/java/com/example/focusflow/SettingsRedirect.java
@@ -0,0 +1,12 @@
+package com.example.focusflow;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsRedirect  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_settings);
+    }
+}
